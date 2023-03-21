900d612cd1e6260da9d19c3e1bc8f4b8444e98bb
cheri <1156429007@qq.com>
2023-03-21 15:47:37 +0800

    模拟头像

--- src/components/Comments/comments.vue
+++ src/components/Comments/comments.vue
@@ -2,7 +2,7 @@
  * @Author: cheri 1156429007@qq.com
  * @Date: 2023-03-21 12:51:01
  * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-03-21 12:51:11
+ * @LastEditTime: 2023-03-21 15:44:05
  * @FilePath: /web3_auction/src/components/Comments/comments.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -52,9 +52,36 @@ import relativeTime from "dayjs/plugin/relativeTime"
 dayjs.extend(relativeTime)
 export default defineComponent({
     setup() {
+        const avatarArray = [
+            "/avatars/avatar_01.jpg",
+            "/avatars/avatar_02.jpg",
+            "/avatars/avatar_03.jpg",
+            "/avatars/avatar_04.jpg",
+            "/avatars/avatar_05.jpg",
+            "/avatars/avatar_06.jpg",
+            "/avatars/avatar_07.jpg",
+            "/avatars/avatar_08.jpg",
+            "/avatars/avatar_09.jpg",
+            "/avatars/avatar_10.jpg",
+            "/avatars/avatar_11.jpg",
+            "/avatars/avatar_12.jpg",
+            "/avatars/avatar_13.jpg",
+            "/avatars/avatar_14.jpg",
+            "/avatars/avatar_15.jpg",
+            "/avatars/avatar_16.jpg",
+            "/avatars/avatar_17.jpg",
+            "/avatars/avatar_18.jpg",
+            "/avatars/avatar_19.jpg",
+            "/avatars/avatar_20.jpg",
+            "/avatars/avatar_21.jpg",
+        ]
         const comments = ref([])
         const submitting = ref(false)
         const value = ref("")
+        function getRandomInt() {
+            return Math.floor(Math.random() * 21)
+        }
+
         const handleSubmit = () => {
             if (!value.value) {
                 return
@@ -64,8 +91,8 @@ export default defineComponent({
                 submitting.value = false
                 comments.value = [
                     {
-                        author: "Han Solo",
-                        avatar: "https://joeschmoe.io/api/v1/random",
+                        author: "Cheri",
+                        avatar: avatarArray[getRandomInt()],
                         content: value.value,
                         datetime: dayjs().fromNow(),
                     },
