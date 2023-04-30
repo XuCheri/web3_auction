b2dfb154588500352e42588fcac4a8ee9c3d5b14
cheri <1156429007@qq.com>
2023-04-30 12:05:25 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
  * @Author: cheri 1156429007@qq.com
  * @Date: 2023-03-28 16:25:55
  * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-04-18 15:31:04
+ * @LastEditTime: 2023-04-28 23:38:42
  * @FilePath: /web3_auction/backend/app.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -85,7 +85,6 @@ app.post("/api/upload", upload.single("avatar"), (req, res) => {
   const filepath = req.file.path;
   res.send(filepath);
 });
-
 // 写一个接口获取全部商品数据
 app.get("/api/getOrders", async (req, res) => {
   const result = await getOrders();
