3885640462171ef97d3af8ec2dac93f1646228e0
cheri <1156429007@qq.com>
2023-05-01 20:11:06 +0800

    fix async bugs

--- frontend/src/components/List/List.vue
+++ frontend/src/components/List/List.vue
@@ -2,7 +2,7 @@
  * @Author: cheri 1156429007@qq.com
  * @Date: 2023-05-01 16:14:00
  * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-05-01 18:26:19
+ * @LastEditTime: 2023-05-01 19:54:29
  * @FilePath: /web3_auction/frontend/src/components/List/List.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -18,11 +18,11 @@
                         <a href="https://www.antdv.com/">{{ item.OrderName }}</a>
                     </template>
                     <template #avatar>
-                        <a-avatar src="https://joeschmoe.io/api/v1/random" />
+                        <a-avatar :src="item.avatarSrc" />
                     </template>
                 </a-list-item-meta>
-                <div class="flex">
-                    <div>
+                <a-row style="margin-bottom: 20px">
+                    <a-col :span="12">
                         拍卖截止时间：{{
                             new Date(new Date(item.AuctionTime).getTime() - 8 * 1000 * 60 * 60)
                                 .toString()
@@ -40,25 +40,35 @@
                                 .toString()
                                 .split(" ")[4]
                         }}
-                    </div>
-                    <div>
+                    </a-col>
+                    <a-col :span="12">
                         合约地址：
                         <a :href="`https://sepolia.etherscan.io/address/` + item.address">{{
                             item.address
                         }}</a>
-                    </div>
-                </div>
-                <div></div>
+                    </a-col>
+                </a-row>
+                <a-row>
+                    <a-col :span="12">
+                        竞拍获胜者：
+                        <a :href="`https://sepolia.etherscan.io/address/` + item.highestBidder">{{
+                            item.highestBidder
+                        }}</a>
+                    </a-col>
+                    <a-col :span="12">竞拍获胜者出价：{{ item.highestBid }} wei</a-col>
+                </a-row>
             </a-list-item>
         </template>
     </a-list>
 </template>
 <script setup>
 const props = defineProps(["data"])
+console.log(props)
 </script>
 <style scoped>
 .flex {
     display: flex;
     justify-content: space-around;
+    margin-bottom: 20px;
 }
 </style>
--- frontend/src/pages/FinishedAuction.vue
+++ frontend/src/pages/FinishedAuction.vue
@@ -2,28 +2,53 @@
  * @Author: cheri 1156429007@qq.com
  * @Date: 2023-04-28 18:04:34
  * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-05-01 18:33:53
+ * @LastEditTime: 2023-05-01 20:04:21
  * @FilePath: /web3_auction/frontend/src/pages/FinishedAuction.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
     <List :data="data" />
+    <a-back-top />
 </template>
 <script setup>
 import List from "@/components/List/List.vue"
 import axios from "axios"
+import { ethers } from "ethers"
 import { onBeforeMount, ref } from "vue"
 onBeforeMount(async () => {
-    await axios.get("http://localhost:3000/api/getOrders").then((res) => {
+    await axios.get("http://localhost:3000/api/getOrders").then(async (res) => {
         res.data = res.data.filter(
             (order) => new Date(order.AuctionTime).getTime() - 8 * 1000 * 60 * 60 - Date.now() < 0
         )
         res.data.sort((a, b) => {
             return new Date(b.AuctionTime).getTime() - new Date(a.AuctionTime).getTime()
         })
+        for (const order of res.data) {
+            await getHighestBidder(order.address, order.abi).then((res) => {
+                order.highestBidder = res
+            })
+            await getValue(order.address, order.abi).then((res) => {
+                order.highestBid = res
+            })
+        }
+        console.log(res.data)
         data.value = res.data
     })
 })
+async function getHighestBidder(address, abi) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = await provider.getSigner()
+    const contract = new ethers.Contract(address, abi, signer)
+    const highestBidder = await contract.highestBidder()
+    return highestBidder
+}
+async function getValue(address, abi) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = await provider.getSigner()
+    const contract = new ethers.Contract(address, abi, signer)
+    const highestBid = await contract.highestBid()
+    return highestBid
+}
 const data = ref([])
 </script>
 <style scoped></style>
