d1b5f7221021ffc1f92747b55db82d09d3cf3a46
cheri <1156429007@qq.com>
2023-05-01 18:35:18 +0800

    basic ui for FinishedAuction

--- frontend/src/components/List/List.vue
+++ frontend/src/components/List/List.vue
@@ -2,24 +2,53 @@
  * @Author: cheri 1156429007@qq.com
  * @Date: 2023-05-01 16:14:00
  * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-05-01 16:17:08
+ * @LastEditTime: 2023-05-01 18:26:19
  * @FilePath: /web3_auction/frontend/src/components/List/List.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <a-list item-layout="horizontal" :data-source="data">
+    <a-list item-layout="vertical" size="large" :data-source="data">
         <template #renderItem="{ item }">
-            <a-list-item>
-                <a-list-item-meta
-                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                >
+            <a-list-item key="item.ID">
+                <template #extra>
+                    <img width="272" height="200" alt="logo" :src="item.imgsrc" />
+                </template>
+                <a-list-item-meta :description="item.OrderDetailDescription">
                     <template #title>
-                        <a href="https://www.antdv.com/">{{ item.title }}</a>
+                        <a href="https://www.antdv.com/">{{ item.OrderName }}</a>
                     </template>
                     <template #avatar>
                         <a-avatar src="https://joeschmoe.io/api/v1/random" />
                     </template>
                 </a-list-item-meta>
+                <div class="flex">
+                    <div>
+                        拍卖截止时间：{{
+                            new Date(new Date(item.AuctionTime).getTime() - 8 * 1000 * 60 * 60)
+                                .toString()
+                                .split(" ")[3] +
+                            " " +
+                            new Date(new Date(item.AuctionTime).getTime() - 8 * 1000 * 60 * 60)
+                                .toString()
+                                .split(" ")[1] +
+                            " " +
+                            new Date(new Date(item.AuctionTime).getTime() - 8 * 1000 * 60 * 60)
+                                .toString()
+                                .split(" ")[2] +
+                            " " +
+                            new Date(new Date(item.AuctionTime).getTime() - 8 * 1000 * 60 * 60)
+                                .toString()
+                                .split(" ")[4]
+                        }}
+                    </div>
+                    <div>
+                        合约地址：
+                        <a :href="`https://sepolia.etherscan.io/address/` + item.address">{{
+                            item.address
+                        }}</a>
+                    </div>
+                </div>
+                <div></div>
             </a-list-item>
         </template>
     </a-list>
@@ -27,4 +56,9 @@
 <script setup>
 const props = defineProps(["data"])
 </script>
-<style scoped></style>
+<style scoped>
+.flex {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
--- frontend/src/pages/FinishedAuction.vue
+++ frontend/src/pages/FinishedAuction.vue
@@ -2,7 +2,7 @@
  * @Author: cheri 1156429007@qq.com
  * @Date: 2023-04-28 18:04:34
  * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-05-01 16:22:49
+ * @LastEditTime: 2023-05-01 18:33:53
  * @FilePath: /web3_auction/frontend/src/pages/FinishedAuction.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -12,25 +12,18 @@
 <script setup>
 import List from "@/components/List/List.vue"
 import axios from "axios"
-import { onBeforeMount } from "vue"
+import { onBeforeMount, ref } from "vue"
 onBeforeMount(async () => {
     await axios.get("http://localhost:3000/api/getOrders").then((res) => {
-        console.log(res.data)
+        res.data = res.data.filter(
+            (order) => new Date(order.AuctionTime).getTime() - 8 * 1000 * 60 * 60 - Date.now() < 0
+        )
+        res.data.sort((a, b) => {
+            return new Date(b.AuctionTime).getTime() - new Date(a.AuctionTime).getTime()
+        })
+        data.value = res.data
     })
 })
-const data = [
-    {
-        title: "Ant Design Title 1",
-    },
-    {
-        title: "Ant Design Title 2",
-    },
-    {
-        title: "Ant Design Title 3",
-    },
-    {
-        title: "Ant Design Title 4",
-    },
-]
+const data = ref([])
 </script>
 <style scoped></style>
